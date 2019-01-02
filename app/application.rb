9c3c9d8a7f3f51ecb4f849375d252a285fa978a5
Mohammed Noori <kmanoorib@gmail.com>
2019-01-02 02:54:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -37,7 +37,19 @@ class Application
     #  resp.status = 404
     # end # end of nested if
     
-    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
     
     resp.finish
   end # end of def call

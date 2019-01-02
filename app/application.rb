0dc807d50df0f8484f9102604d4fc983aae715df
Mohammed Noori <kmanoorib@gmail.com>
2019-01-02 03:06:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,9 +12,9 @@ class Application
     
     @@items = Item.all
     
-    route_match = true if req.path.match(/items/)
-    find_this = req.path.split("/items").last if route_match
-    found_item = ""
+    # route_match = true if req.path.match(/items/)
+    # find_this = req.path.split("/items").last if route_match
+    # found_item = ""
     
     # if route_match
       # check for item name
